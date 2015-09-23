6dc87de470c8d18c3ff7ea88ab293994fb271e75
Luavis <luaviskang@gmail.com>
2015-09-23 20:03:40 +0900

    Implement SOS module system call
    When sos denied access, there are blocking systemcall that sends log messgage to user level program

--- kernel/get_sos_log.c
+++ kernel/get_sos_log.c
@@ -8,10 +8,34 @@
 #include <linux/delay.h>
 #include <linux/slab.h>  // kmalloc
 #include <linux/wait.h>
+#include <linux/mutex.h>
 
+extern struct mutex sos_log_lock;
+extern wait_queue_head_t  sos_wait_log_queue;
+extern char *sos_log_buf;
+extern int sos_log_size;
+extern unsigned char sos_wait_log_cond;
 
 asmlinkage long sys_get_sos_log(char *buf, unsigned long buf_size) {
+    int copy_size = buf_size;
 
+    mutex_lock_interruptible(&sos_log_lock);
 
+    if(sos_log_buf == NULL) {
+        mutex_unlock(&sos_log_lock);
+        wait_event_interruptible(sos_wait_log_queue, sos_wait_log_cond);
+        mutex_lock_interruptible(&sos_log_lock);
+    }
+
+    copy_size = sos_log_size < buf_size ? sos_log_size : buf_size;
+    copy_to_user(buf, sos_log_buf, copy_size);
+
+    kfree(sos_log_buf);
+    sos_log_buf = NULL;
+    sos_wait_log_cond = false;
+
+    mutex_unlock(&sos_log_lock);
+
+    return copy_size;
 }
 
--- security/sos/sos_core.c
+++ security/sos/sos_core.c
@@ -6,14 +6,25 @@
 #include <linux/sched.h>
 #include <linux/sos_rbac_role.h>
 #include <linux/fs.h>
-
-#define DEBUG_SOS(fmt) printk(fmt)
+#include <linux/mutex.h>
+#include <linux/string.h>
 
 /*
  * Declare
  */
 
+#define SOS_LOG_SIZE 1024
+#define DEBUG_SOS 1
+
+
+DEFINE_MUTEX(sos_log_lock);
+DECLARE_WAIT_QUEUE_HEAD(sos_wait_log_queue);
+char *sos_log_buf = NULL;
+int sos_log_size;
+unsigned char sos_wait_log_cond = false;
+
 int sos_lsm_inode_permission(struct inode *inode, int mask);
+void sos_log(char *fmt, ...);
 
 static struct security_operations sos_lsm_ops = {
     .name = "Sos",
@@ -26,24 +37,32 @@ static struct security_operations sos_lsm_ops = {
  * Implementation
  */
 
+struct ls_role *role;
+
 static __init int sos_lsm_init(void) {
+
+    // log mutex init
+    mutex_init(&sos_log_lock);
 
     // reset current security module
     reset_security_ops();
 
-    DEBUG_SOS("SOS: reset security ops\n");
+    sos_log("SOS: reset security ops\n");
 
     // init roles list..
     roles_init();
-    DEBUG_SOS("SOS: init_roles\n");
+
+    role = create_role("Test");
+    ls_create_file_role(role, 15466509, 0);
+
+    sos_log("SOS: init_roles\n");
 
     if(register_security(&sos_lsm_ops)) {
         // make panic when failed to register security module.
         panic("Can not register module");
     }
 
-    DEBUG_SOS("SOS: register security\n");
-
+    sos_log("SOS: register security\n");
 
     return 0;
 }
@@ -53,17 +72,50 @@ sos_lsm_inode_permission
 (struct inode *inode, int mask) {
 
     uid_t current_uid = current_uid().val;
+    int retval;
 
-    struct ls_role *role = ls_get_role_by_uid(current_uid);
     unsigned long i_ino = inode->i_ino;
+    if(current_uid != 1000)
+        return 0;
 
-    int retval = ls_is_role_allowed_inode(role, i_ino, mask);
+    // struct ls_role *role = ls_get_role_by_uid(current_uid);
+
+    retval = ls_is_role_allowed_inode(role, i_ino, mask);
 
     if(unlikely(retval != 0)) {
-        printk("[SOS] denied!!\n");
+        sos_log("uid %d invalid access to %d\n", current_uid, i_ino);
     }
 
     return retval;
+}
+
+void sos_log(char *msg, ...) {
+    va_list ap;
+    char *buf = kmalloc(SOS_LOG_SIZE, GFP_KERNEL);
+    va_start(ap, msg);
+
+    vscnprintf(buf, SOS_LOG_SIZE, msg, ap);
+
+    va_end(ap);
+
+#ifdef DEBUG_SOS
+    printk(buf);
+#endif
+
+    // lock inturruptible
+
+    mutex_lock_interruptible(&sos_log_lock);
+
+    if(likely(sos_log_buf != NULL)) {
+        kfree(sos_log_buf);  // clean old buffer;
+    }
+
+    sos_log_size = strlen(buf);
+    sos_log_buf = buf;
+
+    sos_wait_log_cond = true;
+    wake_up(&sos_wait_log_queue);
+    mutex_unlock(&sos_log_lock);
 }
 
 // install security module
